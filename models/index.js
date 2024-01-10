8a0de3af20cbd8be5bb94ba9586b8acbb7decc91
Opal sn <59205540+OpalSnellneberger@users.noreply.github.com>
2024-01-09 21:01:21 -0500

    added the Models index.js to link the lists

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require('./User');
+const Restaurant = require('./Restaurant');
+const Comment = require('./Comment');
+const Review = require('./Review');
+
+User.hasMany(Review, {
+    foreignKey: 'user_id',
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Restaurant.hasMany(Review, {
+    foreignKey: 'restaurant_id',
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+Review.belongsTo(Restaurant, {
+    foreignKey: 'restaurant_id',
+});
+Review.hasMany(Comment, {
+    foreignKey: 'review_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+Comment.belongsTo(Review, {
+    foreignKey: 'review_id',
+});  
+  
+module.exports = { User, Gallery, Painting };
