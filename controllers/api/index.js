9c6fc9721f86e7d73d84d880ae6dc72cce5131fd
Opal sn <59205540+OpalSnellneberger@users.noreply.github.com>
2024-01-16 19:55:09 -0500

    post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const userRoutes = require('./userRoutes');
 const apiController = require('./reviewRoutes');
 
 router.use('/users', userRoutes);
-router.post('/storeData', apiController.storeRestaurant);
+router.post('/storeRestaurant', apiController.storeRestaurant);
+router.post('/storeReview', apiController.storeReview);
 
 module.exports = router;
